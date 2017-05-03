1bdfe6b759563e90d38f5cbf92d67858831da089
Vedran Duka <duka.vedran@gmail.com>
2017-05-03 15:17:40 +0200

    change

--- app/src/main/java/com/example/vedranduka/alkotestv2/Main2Activity.java
+++ app/src/main/java/com/example/vedranduka/alkotestv2/Main2Activity.java
@@ -1,14 +1,23 @@
 package com.example.vedranduka.alkotestv2;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -26,6 +35,7 @@ public class Main2Activity extends Activity {
     ArrayList<DrinkData> drinks;
     ArrayAdapter<DrinkData> adapter;
     Button btnNext;
+    Button btnClear;
     UserData ud;
 
 
@@ -42,14 +52,29 @@ public class Main2Activity extends Activity {
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, drinks);
         list.setAdapter(adapter);
 
+        list.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                DrinkData d = (DrinkData) list.getItemAtPosition(position);
+                popUp(d);
+            }
+        });
+
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                double alcQ = Double.parseDouble(alcoholQuantity.getText().toString());
-                double alcP = Double.parseDouble(alcoholPercentage.getText().toString());
-                drinks.add(new DrinkData(drinkName.getText().toString(),alcQ, alcP));
-                adapter.notifyDataSetChanged();
+                popUp(null);
+                }
+
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.clear();
+                drinks.clear();
             }
         });
 
@@ -62,6 +87,53 @@ public class Main2Activity extends Activity {
 
         addTestData();
 
+    }
+
+    public void popUp(DrinkData d) {
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.pop_up_layout, null);
+        drinkName = (EditText) alertLayout.findViewById(R.id.pop_drinkNameTextBox);
+        alcoholQuantity = (EditText) alertLayout.findViewById(R.id.pop_alcoholQuantityTextBox);
+        alcoholPercentage = (EditText) alertLayout.findViewById(R.id.pop_alcoholPercentageTextBox);
+
+        if(d != null)
+        {
+            drinkName.setText(d.Name);
+            alcoholQuantity.setText(Double.toString(d.amountDrinking));
+            alcoholPercentage.setText(Double.toString(d.alcoholPercentage));
+        }
+
+
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(Main2Activity.this);
+        alert.setTitle("Drink input");
+        // this is set the view from XML inside AlertDialog
+        alert.setView(alertLayout);
+        // disallow cancel of AlertDialog on click of back button and outside touch
+        alert.setCancelable(false);
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getBaseContext(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alert.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String dN = drinkName.getText().toString();
+                String aQ = alcoholQuantity.getText().toString();
+                String aP = alcoholPercentage.getText().toString();
+                double alcQ = Double.parseDouble(alcoholQuantity.getText().toString());
+                double alcP = Double.parseDouble(alcoholPercentage.getText().toString());
+                drinks.add(new DrinkData(drinkName.getText().toString(),alcQ, alcP));
+                adapter.notifyDataSetChanged();
+            }
+        });
+        AlertDialog dialog = alert.create();
+        dialog.show();
     }
 
     private void addTestData() {
@@ -84,11 +156,10 @@ public class Main2Activity extends Activity {
         userTimeSpentDrinking = (TextView) findViewById(R.id.textViewTimeSpentDrinking);
         userTimeSpentAfter = (TextView) findViewById(R.id.textViewTimeSpentAfter);
         drinkName = (TextView) findViewById(R.id.drinkNameTextBox);
-        alcoholQuantity = (TextView) findViewById(R.id.alcoholQuantityTextBox);
-        alcoholPercentage = (TextView) findViewById(R.id.alcoholPercentageTextBox);
         list = (ListView) findViewById(R.id.list);
         btnAdd = (Button) findViewById(R.id.addButton);
         btnNext = (Button) findViewById(R.id.btnNext);
+        btnClear = (Button) findViewById(R.id.btnClear);
     }
 
     @Nullable
